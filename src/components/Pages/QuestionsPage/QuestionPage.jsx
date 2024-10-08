cd4ef299b6f1c12707349d86b03f28c0dc1ff3e3
unknown <[aurelius.bd123@gmail.com]>
2024-10-09 00:18:15 +0700

    question page added

--- src/components/Pages/QuestionsPage/QuestionPage.jsx
+++ src/components/Pages/QuestionsPage/QuestionPage.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import styles from '../QuestionsPage/QuestionPage.module.scss'
+import Button from '../../common/Button/Button'
 
 export default function QuestionPage() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <div className={styles.header}>
+        <p>Question 1</p>
+        <p>01 : 30</p>  
+    </div>
+
+    <div className={styles.questionCard}>
+        <p className={styles.question}>Who's not member of Akatsuki
+        </p>
+
+        <div className={styles.options}>
+
+                <Button  width='250px' height='100px' backgroundColor='#9C5C5C' color='#ffffff' className={styles.button}>Sasuke Uchiha</Button>
+
+                <Button  width='250px' height='100px' backgroundColor='#9C5C5C' color='#ffffff' className={styles.button}>True</Button>
+
+                <Button  width='250px' height='100px' backgroundColor='#9C5C5C' color='#ffffff' className={styles.button}>True</Button>
+
+                <Button  width='250px' height='100px' backgroundColor='#9C5C5C' color='#ffffff' className={styles.button}>True</Button>
+
+
+        </div>
+
+
+    </div>
+    </>
+
+    
   )
 }
